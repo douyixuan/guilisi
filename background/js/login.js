1c6869ba7f0e6adcd949140c0d98cc98f8ae7d9a
douyixuan <mengxiangyu0826@163.com>
2018-04-04 00:02:34 +0800

    Login complete
    After I set vhost, and learned some jquery,json,etc, I finish the login module.

---
+++ background/js/login.js
@@ -0,0 +1,38 @@
+function fillcheck(f) {
+	var flag=false;
+	var username=f.username.value;
+	var password=f.password.value;
+	//alert(username);
+	//add some fill warning
+	if(username != "" && password != "" && username != null && password != null){
+		if(valid(f)){
+			flag = true;
+		}
+	}
+	//alert(flag);
+	
+	//add some no users tips
+	
+	return flag;
+}
+
+function valid(f) {
+	var username=f.username.value;
+	var password=f.password.value;
+	var data = {};
+	data["username"]=username;
+	data["password"]=password;
+	
+	
+	$.ajax({
+		type: "POST",
+		url:"php/login.php",
+		data:data,
+		timeout:2000,
+		cache:true,
+		success: function (data) {
+			if(data == "uptosky") window.location.href="dashboard.html";
+			else alert("账号或密码错误");
+		}
+	});
+}
---
+++ background/php/login.php
@@ -0,0 +1,62 @@
+<?php
+	$zhanghao=$_POST["username"];
+	$mima=$_POST["password"];
+	echo $text;
+	$servername = "...";
+	$username = "..";
+	$password = "...";
+	$db = "...";
+	
+	// 创建连接
+	$conn = mysqli_connect($servername, $username, $password, $db);
+	 
+	// 检测连接
+	if (!$conn) {
+		die("Connection failed: " . mysqli_connect_error());
+	}
+	//echo "连接成功\n";	// some code  username, password
+	
+	//select db
+		/*if (!mysqli_select_db($conn,'guilisi')) {
+			echo 'error('.mysqli_errno($conn).'):'.mysqli_error($conn);
+			mysqli_close($conn);
+			die;
+		}
+		else {
+			echo "guilisi\n";
+		}*/
+	
+	//set charset
+	mysqli_set_charset($conn,'utf8');
+	
+	//query
+	$sql = "SELECT * FROM users";
+	$result = mysqli_query($conn, $sql);
+	//the result's status
+	//var_dump($result); 
+	
+	/*
+	mysqli_fetch_row：获取一条数据的索引数组
+	mysqli_fetch_assoc：获取一条数据的关联数组
+	mysqli_fetch_array：获取一条数据的指定数组，
+	*/
+	$arr = null;
+	if ($result && mysqli_num_rows($result)>0) {
+		// 输出数据
+		$i = 0;
+		while($row = mysqli_fetch_assoc($result)) {
+			$arr[$row["username"]]=$row["password"];
+			//echo $row["username"]." ".$row["password"]."\n";
+		}
+	}
+	
+	//json encode
+	//echo json_encode($arr);
+	//$text = '{"username":"admin","password":"admin"}';
+	
+	//var_dump($log);
+	//print $log->{"password"};
+	echo $mima==$arr[$zhanghao]?"uptosky":"downtoground";
+	//response.write("</table>");
+	mysqli_close($conn);
+?>
